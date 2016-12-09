d9a81f34522795cd234eeaf285b279314afc6538
Grucholek <winkiel70@gmail.com>
2016-12-09 02:26:14 +0100

    Fix

--- theme/index.php
+++ theme/index.php
@@ -6,174 +6,164 @@
   <title><?php echo e(bloginfo('name')); ?></title>
   <meta name="description" content="">
   <meta name="keyword" content="">
-
+  
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
   <link rel="stylesheet" href="http://localhost:8000/wp-content\themes\ably\css\style.css">
   <?php do_action(wp_enqueue_style( 'style-name', get_stylesheet_uri())); ?>
-
-
-<?php do_action('wp_head'); ?>
+  
+  <?php do_action('wp_head'); ?>
 </head>
 
 <body>
 
-<div class="content">
+  <div class="content">
 
-  <nav class="nav">
-  <div class="nav-left">
-    <a class="nav-item is-brand" href="#">
-      <img src="/wp-content/themes/ably/images/HeyU.png" alt="HeyU logo">
-    </a>
+    <nav class="nav">
+      <div class="nav-left">
+        <a class="nav-item is-brand" href="#">
+          <img src="/wp-content/themes/ably/images/HeyU.png" alt="HeyU logo">
+        </a>
+      </div>
+
+      <div class="nav-right nav-menu">
+        <a class="nav-item" href="#">
+          ABOUT
+        </a>
+        <a class="nav-item" href="#">
+          FAQ
+        </a>
+        <a class="nav-item" href="#">
+          SUPPORT
+        </a>
+        <a class="nav-item" href="#">
+          BLOG
+        </a>
+
+
+      </div>
+    </nav>
+
+    <div id="simple-safe">
+      <div class="left"><b><p id="simple-big">Simple, safe<br>& beautiful.</p></b></h1><br><p id="with">With <b>HeyU</b>, you'ill get fast, simple, secure messaging!</p>
+        <div class="s-button"></div>
+      </div>
+
+      <div id="iphone"></div>
+    </div>
+
+    <div class="clear"></div>
+
+    <div id="easy-messaging">
+      <div id="messages"></div>
+      <div id="top-right-text"><p class="title"><b>Easy Messaging<font color="#E83631">.</b></font></p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin condimentum elit non augue rhoncus, sit amet convallis leo luctus. Nulla sed sagittis urna. Cras rhoncus ultrices sem, ac feugiat lorem tristique sed. Nam egestas nibh vitae sapien dignissim, sed varius arcu efficitur. Phasellus urna augue, ultricies eget ipsum nec, congue lobortis est. Morbi vitae rhoncus eros.</p>
+      </div>
+    </div>
+
+    <div class="clear"></div>
+
+    <div id="keep-in-touch">
+      <div id="top-left-text"><p class="title"><b>Keep in touch<font color="#E83631">.</b></font></p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin condimentum elit non augue rhoncus, sit amet convallis leo luctus. Nulla sed sagittis urna. Cras rhoncus ultrices sem, ac feugiat lorem tristique sed. Nam egestas nibh vitae sapien dignissim, sed varius arcu efficitur. Phasellus urna augue, ultricies eget ipsum nec, congue lobortis est. Morbi vitae rhoncus eros.</p></div>
+        <div id="messages-2"></div>
+      </div>
+
+      <div class="clear"></div>
+
+      <hr>
+      <div id="box-container">
+        <div id="what-people-say"><p class="title text-center"><b>What people say<font color="#E83631">.</b></font></p></div>
+        <div class="m-box">
+          <div class="msg-box">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus molestie justo in eros egestas placerat. Aenean mattis, ante nec dictum sodales, ex dui porttitor nulla, a tincidunt orci nunc non ante. Pellentesque vel mollis lacus. Nunc et justo ut odio tincidunt rhoncus nec eleifend quam.
+            <p class="company">Booking.com</p>
+          </div>
+          <div class="author">
+            <div class="author-img-1"></div>
+            <div class="author-name-1">
+              <b>John Doe</b><br/>
+              CEO
+            </div>
+          </div>
+        </div>
+        <div class="m-box">
+          <div class="msg-box mid-box">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus molestie justo in eros egestas placerat. Aenean mattis, ante nec dictum sodales, ex dui porttitor nulla, a tincidunt orci nunc non ante. Pellentesque vel mollis lacus. Nunc et justo ut odio tincidunt rhoncus nec eleifend quam.
+            <p class="company">Booking.com</p>
+          </div>
+
+          <div class="author mid-box-2">
+           <div class="author-img-1 mid-author"></div>
+           <div class="author-name-1">
+             <b>John Doe</b><br/>
+             CEO
+           </div>
+         </div>
+       </div>
+       <div class="m-box">
+        <div class="msg-box">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus molestie justo in eros egestas placerat. Aenean mattis, ante nec dictum sodales, ex dui porttitor nulla, a tincidunt orci nunc non ante. Pellentesque vel mollis lacus. Nunc et justo ut odio tincidunt rhoncus nec eleifend quam.
+          <p class="company">Booking.com</p>
+        </div>
+
+        <div class="author">
+          <div class="author-img-1"></div>
+          <div class="author-name-1">
+            <b>John Doe</b><br/>
+            CEO
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="clear"></div>
   </div>
+</div>
+</div>
+</div>
+</div>  
+</div>
 
-  <div class="nav-right nav-menu">
-    <a class="nav-item" href="#">
-      ABOUT
-    </a>
-    <a class="nav-item" href="#">
-      FAQ
-    </a>
-    <a class="nav-item" href="#">
-      SUPPORT
-    </a>
-    <a class="nav-item" href="#">
-      BLOG
-    </a>
-
-  
+<div id="capabilities">
+  <div class="s-box left">
+    <div class="s-title">Start conversation</div>
+    <div class="s-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis congue sapien ipsum, vel pellentesque ex lobortis at.</div>
+    <div class="s-img s-message"></div>
   </div>
-</nav>
-
-	<div id="simple-safe">
-	<div class="left"><b><p id="simple-big">Simple, safe<br>& beautiful.</p></b></h1><br><p id="with">With <b>HeyU</b>, you'ill get fast, simple, secure messaging!</p>
-  <div class="s-button"></div>
+  <div class="s-box right">
+    <div class="s-title">Make a free call</div>
+    <div class="s-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis congue sapien ipsum, vel pellentesque ex lobortis at.</div>
+    <div class="s-img s-phone"></div>
   </div>
-
-  <div id="iphone"></div>
-	</div>
 
   <div class="clear"></div>
 
-  <div id="easy-messaging">
-  <div id="messages"></div>
-  <div id="top-right-text"><p class="title"><b>Easy Messaging<font color="#E83631">.</b></font></p>
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin condimentum elit non augue rhoncus, sit amet convallis leo luctus. Nulla sed sagittis urna. Cras rhoncus ultrices sem, ac feugiat lorem tristique sed. Nam egestas nibh vitae sapien dignissim, sed varius arcu efficitur. Phasellus urna augue, ultricies eget ipsum nec, congue lobortis est. Morbi vitae rhoncus eros.</p>
+  <div class="s-box left">
+    <div class="s-title">Take a picture</div>
+    <div class="s-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis congue sapien ipsum, vel pellentesque ex lobortis at.</div>
+    <div class="s-img s-camera"></div>
   </div>
+  <div class="s-box right">
+    <div class="s-title">Send your video</div>
+    <div class="s-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis congue sapien ipsum, vel pellentesque ex lobortis at.</div>
+    <div class="s-img s-camera_2"></div>
   </div>
+</div>
 
-  <div class="clear"></div>
 
-  <div id="keep-in-touch">
-  <div id="top-left-text"><p class="title"><b>Keep in touch<font color="#E83631">.</b></font></p>
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin condimentum elit non augue rhoncus, sit amet convallis leo luctus. Nulla sed sagittis urna. Cras rhoncus ultrices sem, ac feugiat lorem tristique sed. Nam egestas nibh vitae sapien dignissim, sed varius arcu efficitur. Phasellus urna augue, ultricies eget ipsum nec, congue lobortis est. Morbi vitae rhoncus eros.</p></div>
-  <div id="messages-2"></div>
-  </div>
+<p class="title text-center"><b>Subscribe to our newsletter<font color="#E83631">.</b></font></p>
+<p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis congue sapien ipsum, vel pellentesque ex lobortis at.</p>
 
-<div class="clear"></div>
+<div id="subscribe"></div>
 
 <hr>
-<div id="box-container">
-   <div id="what-people-say"><p class="title text-center"><b>What people say<font color="#E83631">.</b></font></p></div>
-   <div class="m-box">
-   <div class="msg-box">
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus molestie justo in eros egestas placerat. Aenean mattis, ante nec dictum sodales, ex dui porttitor nulla, a tincidunt orci nunc non ante. Pellentesque vel mollis lacus. Nunc et justo ut odio tincidunt rhoncus nec eleifend quam.
-<p class="company">Booking.com</p>
-   </div>
 
-   <div class="author">
-   <div class="author-img-1"></div>
-   <div class="author-name-1">
-   <b>John Doe</b><br/>
-   CEO
-   </div>
-</div>
-</div>
-   <div class="m-box">
-   <div class="msg-box mid-box">
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus molestie justo in eros egestas placerat. Aenean mattis, ante nec dictum sodales, ex dui porttitor nulla, a tincidunt orci nunc non ante. Pellentesque vel mollis lacus. Nunc et justo ut odio tincidunt rhoncus nec eleifend quam.
-<p class="company">Booking.com</p>
-   </div>
+<div id="h-icon"></div>
+<p class="title text-center"><b>Download the app<font color="#E83631">.</b></font></p>
+<p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis congue sapien ipsum, vel pellentesque ex lobortis at.</p>
+<div id="android"></div>
 
-   <div class="author mid-box-2">
-   <div class="author-img-1 mid-author"></div>
-   <div class="author-name-1">
-   <b>John Doe</b><br/>
-   CEO
-   </div>
-</div>
-</div>
-   <div class="m-box">
-   <div class="msg-box">
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus molestie justo in eros egestas placerat. Aenean mattis, ante nec dictum sodales, ex dui porttitor nulla, a tincidunt orci nunc non ante. Pellentesque vel mollis lacus. Nunc et justo ut odio tincidunt rhoncus nec eleifend quam.
-<p class="company">Booking.com</p>
-   </div>
-
-   <div class="author">
-   <div class="author-img-1"></div>
-   <div class="author-name-1">
-   <b>John Doe</b><br/>
-   CEO
-   </div>
-</div>
-</div>
-
-</div>
-  <div class="clear"></div>
-
-  </div>
-
-
-
-</div>
-
-   </div>
-
-   </div>
-
-  </div>  
-</div>
-
-  <div id="capabilities">
-  <div class="s-box left">
-  <div class="s-title">Start conversation</div>
-  <div class="s-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis congue sapien ipsum, vel pellentesque ex lobortis at.</div>
-  <div class="s-img s-message"></div>
-  </div>
-  <div class="s-box right">
-  <div class="s-title">Make a free call</div>
-  <div class="s-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis congue sapien ipsum, vel pellentesque ex lobortis at.</div>
-  <div class="s-img s-phone"></div>
-  </div>
-
-  <div class="clear"></div>
-
-  <div class="s-box left">
-  <div class="s-title">Take a picture</div>
-  <div class="s-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis congue sapien ipsum, vel pellentesque ex lobortis at.</div>
-  <div class="s-img s-camera"></div>
-  </div>
-  <div class="s-box right">
-  <div class="s-title">Send your video</div>
-  <div class="s-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis congue sapien ipsum, vel pellentesque ex lobortis at.</div>
-  <div class="s-img s-camera_2"></div>
-  </div>
-  </div>
-
-
-  <p class="title text-center"><b>Subscribe to our newsletter<font color="#E83631">.</b></font></p>
-  <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis congue sapien ipsum, vel pellentesque ex lobortis at.</p>
-
-  <div id="subscribe"></div>
-
-  <hr>
-
-  <div id="h-icon"></div>
-  <p class="title text-center"><b>Download the app<font color="#E83631">.</b></font></p>
-  <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis congue sapien ipsum, vel pellentesque ex lobortis at.</p>
-  <div id="android"></div>
-
-  <div class="border"></div>
-  <nav class="nav">
+<div class="border"></div>
+<nav class="nav">
   <div class="nav-left nav-menu">
     <a class="nav-item" href="#">
       ABOUT
@@ -187,17 +177,15 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus molestie justo 
     <a class="nav-item" href="#">
       BLOG
     </a>
-  <div class="nav-right">
-    <a class="nav-item lang" href="#">
-    </a>
+    <div class="nav-right">
+      <a class="nav-item lang" href="#">
+      </a>
+    </div>
+  </nav>
 
   
-  </div>
-</nav>
-
-
-<script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
+  <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  
 </body>
 </html>
