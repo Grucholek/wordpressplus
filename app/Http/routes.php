217092b592ef7fc0aab922ff647fa06f024e0cd6
Grucholek <winkiel70@gmail.com>
2016-12-14 12:15:04 +0100

    Api

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Http\Request;
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -10,5 +12,35 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+
+$app->get('/api/post', function(Request $request){
+
+  $id =  $request->input('id');
+  $slug = $request->input('slug');
+  $from = $request->input('from');
+  $to = $request->input('to');
+  $first_time = "1973-01-01 00:00:00";
+
+  if(isset($id))
+  {
+  	return $results = app('db')->select('select * from wp_posts where id = :id', ['id' => $id]);
+  }
+  elseif(isset($slug))
+  {
+  	return $results = app('db')->select('select * from wp_posts where post_name = :slug', ['slug' => $slug]);
+  }
+  elseif(isset($from) && empty($to))
+  {
+  	return $results = app('db')->select('select * from wp_posts where post_date between :from AND NOW()', ['from' => $from]);
+  }
+  elseif(isset($to) && empty($from))
+  {
+  	return $results = app('db')->select('select * from wp_posts where post_date between :first_time AND :to', ['first_time' => $first_time, 'to' => $to]);
+  }
+  elseif(isset($to) && isset($from))
+  {
+  	return $results = app('db')->select('select * from wp_posts where post_date between :from AND :to', ['from' => $from, 'to' => $to]);
+  }
+});
 
 require __DIR__.'/wp-routes.php';
